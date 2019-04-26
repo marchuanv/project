950f120e2853a7044c70e8c2baa4177ddff9082a
marchuanv <marchuanv@gmail.com>
2019-04-26 03:20:33 +0200

    fix

--- checkout.js
+++ checkout.js
@@ -14,39 +14,39 @@ const octokit = new Octokit({
 const gitIgnorePath = join(__dirname,".gitignore");
 const gitIgnoreContent = readFileSync(gitIgnorePath,"utf8")
 
+const downloadFiles=(owner, repo, path)=>{
+    octokit.repos.getContents({ owner, repo, path}).then(async({ data }) => {
+        for(const metadata of data){
+            const newFilePath = join(__dirname, repo, metadata.path);
+            if (existsSync(newFilePath)===false){
+                if (metadata.type === "dir"){
+                    console.log("creating directory: ",newFilePath);
+                    mkdirSync(newFilePath);
+                    downloadFiles(owner, repo, metadata.path);
+                }
+                if (metadata.type === "file"){
+                    console.log("creating file: ",newFilePath);
+                    const blob = await octokit.gitdata.getBlob({ owner, repo, file_sha: metadata.sha });
+                    const buff = new Buffer(blob.data.content, 'base64');
+                    writeFileSync(newFilePath,buff.toString('ascii'));
+                }
+            }
+        };
+    });
+};
+
 octokit.repos.listForUser({
   username: owner
 }).then(({ data }) => {
     for(const repoName of data.map(x=>x.name)){
-        const repoFilePath = join(__dirname, repoName);
-        if (existsSync(repoFilePath)===false){
+        if (existsSync(join(__dirname, repoName))===false){
             mkdirSync(repoName);
             const ignorePhrase=`\n${repoName}/`;
             if (gitIgnoreContent.indexOf(ignorePhrase)===-1) {
                 appendFileSync(gitIgnorePath, ignorePhrase);
             }
         }
-        octokit.repos.getContents({
-          owner,
-          repo: repoName,
-          path: ""
-        }).then(async({ data }) => {
-            for(const metadata of data){
-                const newFilePath = join(__dirname, repoName, metadata.path);
-                if (existsSync(newFilePath)===false){
-                    if (metadata.type === "dir"){
-                        console.log("creating directory: ",newFilePath);
-                        mkdirSync(newFilePath);
-                    }
-                    if (metadata.type === "file"){
-                        console.log("creating file: ",newFilePath);
-                        const blob = await octokit.gitdata.getBlob({ owner, repo: repoName, file_sha: metadata.sha });
-                        const buff = new Buffer(blob.data.content, 'base64');
-                        writeFileSync(newFilePath,buff.toString('ascii'));
-                    }
-                }
-            };
-        });
+        downloadFiles(owner, repoName, "");
     };
 });
 
